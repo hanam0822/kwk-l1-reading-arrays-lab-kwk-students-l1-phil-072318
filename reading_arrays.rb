ccb0669200c0f7e5a1bcf7cf76489c8cdbc143d0
hanam0822 <hanam0822@gmail.com>
2018-07-25 14:53:09 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -28,5 +28,5 @@ def last_student_by_method
 end
 
 def first_second_and_third_students
-  return STUDENT_NAMES.first_second_and_third_students
+  return STUDENT_NAMES.(first_second_and_third_students).to eql
 end
